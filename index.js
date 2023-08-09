d556d27f35537411fe4f4174e5df911eedbd9f0d
Davidson Gomes <davidsongviolao@gmail.com>
2023-08-09 12:09:21 -0300

    removed frontend

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import helmet from 'helmet'
-import pino from 'pino-http'
-import exphbs from 'express-handlebars'
-import fs from 'fs'
-import path from 'path'
-import QRCode from 'qrcode'
-import BrCode from './lib/br_code.js';
-import HTMLParser from 'node-html-parser';
+import express from "express";
+import bodyParser from "body-parser";
+import helmet from "helmet";
+import pino from "pino-http";
+import exphbs from "express-handlebars";
+import fs from "fs";
+import path from "path";
+import QRCode from "qrcode";
+import BrCode from "./lib/br_code.js";
+import HTMLParser from "node-html-parser";
 
 const app = express();
 
 app.use(helmet());
 app.use(pino());
 app.use(bodyParser.json());
-app.use(express.static('website/public'))
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+app.use(express.static("website/public"));
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8081;
 const QR_CODE_SIZE = 400;
-const article_links = []
+const article_links = [];
 
 const contentSecurityPolicy = [
   "script-src 'self' 'nonce-2726c7f26c' www.googletagmanager.com pagead2.googlesyndication.com",
@@ -31,35 +31,46 @@ app.use((req, res, next) => {
   next();
 });
 
-fs.readdirSync('views/articles/').forEach(file => {
-  var path_article = file.replace(/\.[^/.]+$/, "")
+fs.readdirSync("views/articles/").forEach((file) => {
+  var path_article = file.replace(/\.[^/.]+$/, "");
 
-  app.get("/" + path_article, function(req, res) {
-    res.render("articles/" + path_article, { article: true, google_public_id: process.env.google_public_id});
+  app.get("/" + path_article, function (req, res) {
+    res.render("articles/" + path_article, {
+      article: true,
+      google_public_id: process.env.google_public_id,
+    });
   });
 
-  var data = fs.readFileSync('views/articles/' + file);
-
+  var data = fs.readFileSync("views/articles/" + file);
 
   const root = HTMLParser.parse(data);
 
-  article_links.push({"title": root.querySelector('h1').rawText, "path": path_article});
+  article_links.push({
+    title: root.querySelector("h1").rawText,
+    path: path_article,
+  });
 });
 
-app.get('/', function(req, res) {
-  res.render('index', {article_links: article_links, google_public_id: process.env.google_public_id});
+app.get("/", function (req, res) {
+  res.json({
+    message: "Welcome to generate pix server!",
+  });
+  // res.render("index", {
+  //   article_links: article_links,
+  //   google_public_id: process.env.google_public_id,
+  // });
 });
 
-app.post('/emvqr-static', (req, res) => {
-  var { key, amount, name, reference, key_type, city } = req.body
+app.post("/emvqr-static", (req, res) => {
+  var { key, amount, name, reference, key_type, city } = req.body;
 
   if (key) {
-      var brCode = new BrCode(key, amount, name, reference, key_type, city);
+    var brCode = new BrCode(key, amount, name, reference, key_type, city);
 
-      var code = brCode.generate_qrcp()
+    var code = brCode.generate_qrcp();
 
-      QRCode.toDataURL(code, {width: QR_CODE_SIZE, height: QR_CODE_SIZE})
-      .then(qrcode => {
+    QRCode.toDataURL(code, { width: QR_CODE_SIZE, height: QR_CODE_SIZE })
+      .then((qrcode) => {
         res.json({
           qrcode_base64: qrcode,
           code: code,
@@ -69,18 +80,18 @@ app.post('/emvqr-static', (req, res) => {
           name: brCode.name,
           city: brCode.city,
           reference: brCode.reference,
-          formated_amount: brCode.formated_amount()})
+          formated_amount: brCode.formated_amount(),
+        });
       })
-      .catch(err => {
-        console.error(err)
-      })
-  }
-  else {
+      .catch((err) => {
+        console.error(err);
+      });
+  } else {
     res.status(422);
-    res.json({ error: "Campo Key não presente"});
+    res.json({ error: "Campo Key não presente" });
   }
 });
 
 app.listen(port, () => {
-  console.log(`Starting generate pix server on port ${port}!`)
+  console.log(`Starting generate pix server on port ${port}!`);
 });
